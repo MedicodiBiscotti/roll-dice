cd00f997c64fc4d9116d540e4a411b8698abcb1c
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2022-09-11 22:12:39 +0200

    adds simple cli

--- roll.py
+++ roll.py
@@ -1,8 +1,16 @@
+import argparse
 import random as rnd
 
 
 def roll_dice(rolls: int, faces: int) -> list[int]:
     return [rnd.randint(1, faces) for i in range(rolls)]
 
+def main(rolls: int, faces: int) -> None:
+    print(roll_dice(rolls, faces))
+
 if __name__ == "__main__":
-    print(roll_dice(2, 20))
+    parser = argparse.ArgumentParser(description="Rolls dice")
+    parser.add_argument('rolls', type=int, help="Number of dice to roll")
+    parser.add_argument('faces', type=int, help="Number of faces/sides on the die")
+    args = parser.parse_args()
+    main(args.rolls, args.faces)
