0d67891383ff0a532ef296b60376ba6111540e0b
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2022-09-11 20:03:55 +0200

    function to return dice rolls

--- roll.py
+++ roll.py
@@ -1,4 +1,8 @@
+import random as rnd
 
+
+def roll_dice(rolls: int, faces: int) -> list[int]:
+    return [rnd.randint(1, faces) for i in range(rolls)]
 
 if __name__ == "__main__":
-    pass
+    print(roll_dice(2, 20))
