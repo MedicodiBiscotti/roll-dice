0c3737057b519e49ba4bb992ab5ec329f04e4dff
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-05-16 13:04:12 +0200

    cleanup, typos, and formatting

--- modules/utils.py
+++ modules/utils.py
@@ -1,23 +1,25 @@
 from typing import Union
 
+
 # I had an idea for how to extend this shit to all hell. If you could give it a parameter for number of sections to create,
 # then you would also figure out those chunks should be organised most neatly.
 # I figured out some logic for it, but it's way too complicated for this project at the moment.
 
+
 # TODO start end end characters as either str (mirror on other side) or list for both sides, default=None
 # Union can be replaced with str | list[str] in version >= 3.10
-def separater_line(
+def separator_line(
     max_length: int,
-    char: str='=',
-    middle: Union[str, list[str]]=None,
-    buffer: int=2,
-    spacer: Union[str, list[str]]=' ',
-    terminator: Union[str, list[str]]=[]    # default empty list seems a little odd.
+    char: str = "=",
+    middle: Union[str, list[str]] = None,
+    buffer: int = 2,
+    spacer: Union[str, list[str]] = " ",
+    terminator=None,  # default empty list seems a little odd.
 ) -> str:
     """Returns a line of the given character with optional text in the middle.
 
     If middle text is a list, the longest string that can comfortably fit in the length will be used.
-    
+
     If spacer is a string, it will be mirrored on the other side of the mid text.
 
     :param max_length: max length of the line.
@@ -25,47 +27,55 @@ def separater_line(
     :param middle: text to put in the middle of the line, defaults to None.
     :param buffer: minimum length of the line on either side of the middle text, defaults to 2.
     :param spacer: character(s) to use to separate the middle text from the line, defaults to ' '.
+    :param terminator: end characters of the line. If a string, it will be mirrored on the other side.
+    List of 2 elements is allowed for exact control. Defaults to None.
     :return: str
     """
     # if no middle text is given, return simple line
     if not middle:
-        return simple_separater_line(max_length, char)
-    
+        return simple_separator_line(max_length, char)
+
     if isinstance(middle, str):
         middle = [middle]
 
     # sort the middle strings by length, longest first
     middle = sorted(middle, key=len, reverse=True)
-    
+
     # if spacer is a string, make it a list but mirror the second element
     if isinstance(spacer, str):
         spacer = [spacer, spacer[::-1]]
-        
+
+    if terminator is None:
+        terminator = []
+
     # do the same with terminator
     # if we really need to do the same exact thing, then it should be a function.
     # If you do that, make sure to also fix if list is given but with length 1.
     if isinstance(terminator, str):
         terminator = [terminator, terminator[::-1]]
 
-    combined_buffer = buffer*2 + sum(map(len, spacer + terminator))
+    combined_buffer = buffer * 2 + sum(map(len, spacer + terminator))
 
     # find the longest middle string that can fit in the length
     for mid in middle:
         if can_fit(max_length, mid, buffer=combined_buffer):
-            # TODO add terminator before and after the line. Terminator length should be subracted from the fill length.
+            # TODO add terminator before and after the line. Terminator length should be subtracted from the fill length.
             return f"{spacer[0] + mid + spacer[1]:{char}^{max_length}}"
-    
+
     # if no middle string can fit, return simple line
     # I could reuse the code if middle is not given or doesn't fit, but I would have to indent the whole thing if middle is given.
-    return simple_separater_line(max_length, char)
+    return simple_separator_line(max_length, char)
 
 
-def simple_separater_line(max_length: int, char: str='=') -> str:
+""""""
+
+
+def simple_separator_line(max_length: int, char: str = "=") -> str:
     """Returns a simple line of the given character."""
     return char * max_length
 
 
-def can_fit(max_length: int, text: str, buffer: int=6) -> bool:
+def can_fit(max_length: int, text: str, buffer: int = 6) -> bool:
     """Returns True if the text can fit in the max_length with the buffer.
 
     :param max_length: max length of the text plus buffer.
@@ -79,7 +89,7 @@ def can_fit(max_length: int, text: str, buffer: int=6) -> bool:
 if __name__ == "__main__":
     s = "hello world!"
     print(s)
-    print(separater_line(len(s), middle=["greetings", "hi", "hello"]))
-    print(separater_line(len(s), middle=["greetings", "hi", "hello"], spacer="- "))
-    print(separater_line(len(s), middle="bye", char="+", spacer=[" ~", "-> "]))
-    print(separater_line(20, middle="WELCOME", spacer="~- "))
+    print(separator_line(len(s), middle=["greetings", "hi", "hello"]))
+    print(separator_line(len(s), middle=["greetings", "hi", "hello"], spacer="- "))
+    print(separator_line(len(s), middle="bye", char="+", spacer=[" ~", "-> "]))
+    print(separator_line(20, middle="WELCOME", spacer="~- "))
--- roll.py
+++ roll.py
@@ -2,32 +2,36 @@ import argparse
 import random as rnd
 from typing import Callable
 
-from modules.utils import separater_line
+from modules.utils import separator_line
 
 
 def roll_dice(rolls: int, faces: int) -> list[int]:
     return [rnd.randint(1, faces) for i in range(rolls)]
 
 
-def sum_rolls(rolls: int, faces: int, modifier: int) -> None:
+def sum_rolls(rolls: int, faces: int, modifier: int):
     results = roll_dice(rolls, faces)
     sum_result = sum(results) + modifier
     expected_avg = (faces / 2 + 0.5) * rolls + modifier
-    return (results, sum_result, expected_avg)
+    return results, sum_result, expected_avg
 
 
 def advantage(rolls: int, faces: int, modifier: int):
     results = roll_dice(rolls, faces)
     max_result = max(results) + modifier
-    expected_avg = faces * rolls / (rolls + 1) + 0.5 + modifier     # thank you, Matt Parker # m / (m + 1) * n + 0.5
-    return (results, max_result, expected_avg)
+    expected_avg = (
+        faces * rolls / (rolls + 1) + 0.5 + modifier
+    )  # thank you, Matt Parker # m / (m + 1) * n + 0.5
+    return results, max_result, expected_avg
 
 
 def disadvantage(rolls: int, faces: int, modifier: int):
     results = roll_dice(rolls, faces)
     min_result = min(results) + modifier
-    expected_avg = faces / (rolls + 1) + 0.5 + modifier     # thank you, Matt Parker # 1 / (m + 1) * n + 0.5
-    return (results, min_result, expected_avg)
+    expected_avg = (
+        faces / (rolls + 1) + 0.5 + modifier
+    )  # thank you, Matt Parker # 1 / (m + 1) * n + 0.5
+    return results, min_result, expected_avg
 
 
 def raw(rolls: int, faces: int):
@@ -38,45 +42,84 @@ def main(rolls: int, faces: int, op: Callable, modifier: int) -> None:
     if op is raw:
         print(op(rolls, faces))
         return
-    
+
     # adjust for advantage and disadvantage
     if op in [advantage, disadvantage]:
         rolls = max(2, rolls)
-    
+
     # pluralize faces if there are multiple rolls.
-    # f-string doesn't allow backslashes so you have to either define the desired string as a variable, or use docstring
+    # f-string doesn't allow backslashes, so you have to either define the desired string as a variable, or use docstring
     intro_str = f"""Rolling {rolls} d{faces}{"'s"[:2*rolls^2]}{f" with a modifier of {modifier}" if modifier else ""}"""
     print(intro_str)
-    print(separater_line(len(intro_str), middle=['Good luck', 'GL']))
+    print(separator_line(len(intro_str), middle=["Good luck", "GL"]))
 
     raw_results, result, expected_avg = op(rolls, faces, modifier)
-    print("You rolled: " + ', '.join(map(str, raw_results)))        # all this just to get rid of the brackets. perhaps unwarranted.
+    print(
+        "You rolled: " + ", ".join(map(str, raw_results))
+    )  # all this just to get rid of the brackets. perhaps unwarranted.
     print(f"Result: {result}")
-    print(f"With an expected value of {round(expected_avg, 2)}")    # val:.2f will have trailing zeros. :.4g won't but converts to scientific notation if needed.
+    print(
+        f"With an expected value of {round(expected_avg, 2)}"
+    )  # val:.2f will have trailing zeros. :.4g won't but converts to scientific notation if needed.
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="rolls dice")
-    parser.add_argument('rolls', type=int, nargs='?', default=1, help="number of dice to roll (default: 1)")
-    parser.add_argument('faces', type=int, nargs='?', default=20, help="number of faces/sides on the die (default: 20)")
-    parser.add_argument('-m', '--modifier', type=int, default=0, help="modifier to add to the result")
+    parser.add_argument(
+        "rolls",
+        type=int,
+        nargs="?",
+        default=1,
+        help="number of dice to roll (default: 1)",
+    )
+    parser.add_argument(
+        "faces",
+        type=int,
+        nargs="?",
+        default=20,
+        help="number of faces/sides on the die (default: 20)",
+    )
+    parser.add_argument(
+        "-m", "--modifier", type=int, default=0, help="modifier to add to the result"
+    )
 
     arg_group = parser.add_mutually_exclusive_group()
-    arg_group.add_argument('-s', '--sum',
-        dest='op', action='store_const', const=sum_rolls, default=sum_rolls,
-        help="sums the result of the rolls (default)")
+    arg_group.add_argument(
+        "-s",
+        "--sum",
+        dest="op",
+        action="store_const",
+        const=sum_rolls,
+        default=sum_rolls,
+        help="sums the result of the rolls (default)",
+    )
 
-    arg_group.add_argument('-a', '--advantage',# '--adv',
-        dest='op', action='store_const', const=advantage,
-        help="roll with advantage, i.e. take the highest of the rolls")
+    arg_group.add_argument(
+        "-a",
+        "--advantage",  # '--adv',
+        dest="op",
+        action="store_const",
+        const=advantage,
+        help="roll with advantage, i.e. take the highest of the rolls",
+    )
 
-    arg_group.add_argument('-d', '--disadvantage',# '--dis',
-        dest='op', action='store_const', const=disadvantage,
-        help="roll with disadvantage, i.e. take the lowest of the rolls")
+    arg_group.add_argument(
+        "-d",
+        "--disadvantage",  # '--dis',
+        dest="op",
+        action="store_const",
+        const=disadvantage,
+        help="roll with disadvantage, i.e. take the lowest of the rolls",
+    )
 
-    arg_group.add_argument('-r', '--raw',
-        dest='op', action='store_const', const=raw,
-        help="returns the resulting rolls in a list as is")
+    arg_group.add_argument(
+        "-r",
+        "--raw",
+        dest="op",
+        action="store_const",
+        const=raw,
+        help="returns the resulting rolls in a list as is",
+    )
 
     args = parser.parse_args()
 
@@ -84,5 +127,5 @@ if __name__ == "__main__":
     # if not any([args.sum, args.advantage, args.disadvantage]):
     #     args.sum = True
     # print(args)
-    
+
     main(args.rolls, args.faces, args.op, args.modifier)
